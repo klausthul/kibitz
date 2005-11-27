a3e07b42b46d7703146e46cc1656b3fe192236e7
kthul <kthul@57bb5161-3a03-0410-9dac-d362435ef476>
2005-11-27 09:18:27 +0000

    Refactoring started - does not build!
    git-svn-id: http://svn.berlios.de/svnroot/repos/kibitz/trunk@23 57bb5161-3a03-0410-9dac-d362435ef476

--- AppController.h
+++ AppController.h
@@ -6,6 +6,7 @@
 #import "ChessServerListControl.h"
 #import "SeekGraph.h"
 #import "ChessView.h"
+#import "ChessServerConnection.h"
 
 #define min(a, b) (((a) < (b)) ? (a) : (b))
 
@@ -13,33 +14,12 @@
 
 @interface AppController : NSObject
 {
-    IBOutlet NSWindow *mainWindow;
-	IBOutlet NSTextView *serverOutput;
-	IBOutlet NSTextField *serverInput;
-	IBOutlet Game *game;
-	IBOutlet NSWindow *promotionPiece;
-	IBOutlet NSWindow *serverSelect;
-	IBOutlet SeekGraph *seekGraph;
-	IBOutlet ChessServerListControl *chessServerListControl;
-	IBOutlet NSDrawer *seekDrawer;
-	NSInputStream *serverIS;
-	NSOutputStream *serverOS;
-	char lineBuf[4096];
-	char move[10];
-	int lastChar;
-	ChessServer *currentServer;
-	bool sendNamePassword, sendInit;
-	NSTimer *timer;
+	ChessServerConnection *chessServerConnection;
+	ChessServerListControl *chessServerListControl;
 }
 
-- (NSSize)windowWillResize:(NSWindow *)sender toSize:(NSSize)proposedFrameSize;
-- (void) userMoveFrom: (ChessField) from to: (ChessField) to;
-- (void) dealloc;
-- (IBAction) selectedPromotionPiece: (id) sender;
-- (IBAction) selectServer: (id) sender;
-- (IBAction) finishServerSelection: (id) sender;
-- (IBAction) toggleSeekDrawer: (id) sender;
-- (void) updateClock: (NSTimer *) aTimer;
 + (void) initialize;
+- (void) dealloc;
+- (IBAction) selectServer: (id) sender;
 
 @end
